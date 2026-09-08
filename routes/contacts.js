346a357d11c52346bc1fba1a32f64a1275781f2c
laurelcerrato <lcerratoramirez@byupathway.edu>
2026-09-08 15:11:38 -0600

    connected  contacts database

---
+++ routes/contacts.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const contactsController = require('../controllers/contacts.js');
+router.get('/', contactsController.getAll);
+router.get('/:id', contactsController.getSingle);
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
 const router = require('express').Router();
-router.get('/', (req, res) => (res.send("hello World")));
+//router.get('/', (req, res) => (res.send("hello World")));
+router.use('/contacts', require('./contacts'));
 module.exports = router;
