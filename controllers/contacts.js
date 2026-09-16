2daf148304b7e9efe615c19cc4a083f00d27eeb6
laurelcerrato <lcerratoramirez@byupathway.edu>
2026-09-15 19:01:36 -0600

    fixed error

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -19,12 +19,13 @@ const getSingle = async (req, res) => {
 };
 
 const createContact = async (req, res) => {
-    //swagger-tags=['Contacts']
+    //swagger-tags=["Contacts"]
     const contact = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
         email: req.body.email,
-        username: req.body.username,
-        name: req.body.name,
-        ipaddress: req.body.ipaddress
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday,
     };
     const response = await mongodb.getDatabase().db().collection('contacts').insertOne(contact);
     if (response.acknowledged) {
@@ -34,13 +35,14 @@ const createContact = async (req, res) => {
     }
 };
 const updateContact = async (req, res) => {
-    //swagger-tags=['Contacts']
+    //swagger-tags=["Contacts"]
     const contactId = new ObjectId(req.params.id);
     const contact = {
-        username: req.body.username,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
         email: req.body.email,
-        name: req.body.name,
-        ipaddress: req.body.ipaddress
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday,
     };
     const response = await mongodb.getDatabase().db().collection('contacts').replaceOne({ _id: contactId }, contact);
     if (response.modifiedCount > 0) {
